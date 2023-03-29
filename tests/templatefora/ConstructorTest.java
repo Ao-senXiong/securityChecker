b257052d22b2022c370d28dffa15401cc3f58a18
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-03-29 15:29:38 -0400

    modify test cases

--- tests/templatefora/ConstructorTest.java
+++ tests/templatefora/ConstructorTest.java
@@ -1,2 +1,27 @@
-package templatefora;public class ConstructorTest {
+package templatefora;
+
+import org.checkerframework.checker.security.qual.*;
+
+public class ConstructorTest {
+
+    private int testField;
+
+    public ConstructorTest(int s) {
+        this.testField = s;
+    }
+
+    public ConstructorTest(@Safe int s, Integer dummy) {
+        this.testField = s;
+    }
+
+    public ConstructorTest(@Trusted int s, String dummy) {
+        this.testField = s;
+    }
+
+    ConstructorTest object1 = new ConstructorTest(0);
+    // :: error: argument
+    ConstructorTest object2 = new ConstructorTest(1, 1);
+    // :: error: argument
+    ConstructorTest object3 = new ConstructorTest(2, "2");
+
 }
--- tests/templatefora/FlowTest.java
+++ tests/templatefora/FlowTest.java
@@ -1,2 +1,16 @@
-package templatefora;public class FlowTest {
+package templatefora;
+
+import org.checkerframework.checker.security.qual.*;
+
+
+public class FlowTest {
+    void test1(@Safe Object a, @Trusted Object b) {
+        @Trusted Object x = a;
+        x = b;
+    }
+
+    void test2(@Safe int a, @Trusted int b) {
+        // :: error: assignment
+        @Safe int x = a + b;
+    }
 }
